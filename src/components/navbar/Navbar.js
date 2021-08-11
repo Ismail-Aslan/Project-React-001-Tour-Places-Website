b44145c7bb77df8096831828ec5bacedeaca1e4f
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-11 09:45:25 +0300

    navbar created

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+
+function Navbar(props) {
+
+    
+
+
+    return(
+        <div className={styles["navbar"]}>
+            <div >
+                <a href="#" className={styles["navbar-item"]}>About Us</a>
+                <a href="#" className={styles["navbar-item"]}>For you</a>
+                <a href="#" className={styles["navbar-item"]}>Services</a>
+                <a href="#" className={styles["navbar-item"]}>Blog</a>
+                <a href="#" className={styles["navbar-item"]}>Vlog</a>
+                <a href="#" className={styles["navbar-item"]}>Contact</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
