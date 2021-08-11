adc783b74b22ccc6fec8907c9d8fee3aa1d51c29
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-11 09:44:33 +0300

    cards created

---
+++ src/components/cards/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Card.module.css"
+
+function Card(props) {
+    
+
+
+    return(
+        <div className={styles["card-frame"]}>
+            <h2 className={styles.placeHeader}>{props.placeName}</h2>
+            <img src={props.placeImgSrc} alt={props.placeImgAlt} className={styles.placeImg}/>
+            <div className={styles["overlay"]}>
+                <p className={styles["text"]}>{props.placeText}</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Card;
